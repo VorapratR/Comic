97de2e7dad6299029b4f5a04421a6f9b99614266
sagolwong <5910110678@psu.ac.th>
2018-11-12 00:34:50 +0700

    UI : เพิ่มปุ่ม logout

--- Comic/src/pages/main/main.ts
+++ Comic/src/pages/main/main.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Tab1Page } from '../tab1/tab1';
 import { Tab2Page } from '../tab2/tab2';
 import { Tab3Page } from '../tab3/tab3';
+import { AlertController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the MainPage page.
@@ -22,11 +24,32 @@ export class MainPage {
   tab2Page = Tab2Page;
   tab3Page = Tab3Page;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alerCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MainPage');
+  }
+  doConfirm() {
+    let confirm = this.alerCtrl.create({
+      title: 'Sure to Logout?',
+      message: 'Do you agree to logout?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            this.navCtrl.push(HomePage)
+          }
+        }
+      ]
+    });
+    confirm.present()
   }
 
 }
