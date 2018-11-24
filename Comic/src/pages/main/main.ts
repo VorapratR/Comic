5679a80cccbc84495ceaa239358f39458d4b86e1
thipmanus <32326290+thipmanus@users.noreply.github.com>
2018-11-24 07:09:30 +0700

    แก้ไข ไอคอนอัปโหลด บัค เปลี่ยนวิธีเขียนแปลภาษา อีกเยอะแยะๆๆ

--- Comic/src/pages/main/main.ts
+++ Comic/src/pages/main/main.ts
@@ -1,9 +1,9 @@
-import { Tab4Page } from './../tab4/tab4';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Tab1Page } from '../tab1/tab1';
 import { Tab2Page } from '../tab2/tab2';
 import { Tab3Page } from '../tab3/tab3';
+import { Tab4Page } from '../tab4/tab4';
 import { AlertController } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
